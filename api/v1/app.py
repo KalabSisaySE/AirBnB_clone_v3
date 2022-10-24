20380698b043fc5e1d1b4930921a8777806bb03b
KalabSisaySE <kyimusic11@gmail.com>
2022-10-24 19:07:14 +0000

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""the api's main application"""
-
-from flask import Flask, jsonify, make_response
+"""app.py to connect to API"""
+import os
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """handles the `404` error"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0") 
-    port = int(getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, debug=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
